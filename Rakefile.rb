b224d5dc22155f11e07fa344559c35a4e1ffe70e
Oskar Gewalli <oskar@gewalli.se>
2014-02-17 09:50:42 +0100

    Rake tasks

--- Rakefile.rb
+++ Rakefile.rb
@@ -8,3 +8,25 @@ task :test => :spec
 
 require 'bundler/gem_helper'
 Bundler::GemHelper.install_tasks({:dir=>File.dirname(__FILE__),:name=>'fruit_to_lime'})
+
+
+task :install_fruit_to_lime do
+    sh "bundle install"
+end
+
+require 'rubygems'
+require 'rubygems/gem_runner'
+require 'rubygems/exceptions'
+
+task :uninstall_fruit_to_lime do
+    ['fruit_to_lime','fruit-to-lime'].each do |tool|
+        begin
+            Gem::GemRunner.new.run ['uninstall', tool, '-a', '-x']
+        rescue Gem::SystemExitException => e
+            puts e
+        end
+    end
+end
+
+desc "test fruit to lime gem"
+task :test_fruit_to_lime => [:uninstall_fruit_to_lime, :install_fruit_to_lime, :spec] 
