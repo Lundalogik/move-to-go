24c2c818aa4bf9c023cc838d3a73bddffd2efabf
Oskar Gewalli <oskar@gewalli.se>
2014-04-10 12:29:08 +0200

    Should be a bit clearer

--- spec/templating_spec.rb
+++ spec/templating_spec.rb
@@ -1,11 +1,11 @@
 require 'fileutils'
 require 'tmpdir'
 
-def execute_command_with_success(cmd)
+def execute_command_with_success_for_template(cmd, template)
 	system(cmd)
 	if ! $?.success?
 		puts "Failed with #{$?}"
-		raise "failed! #{cmd}"
+		raise "failed! #{cmd} for template #{template}"
 	end
 end
 
@@ -29,7 +29,7 @@ describe 'Templating' do
 			templating.list().each {|t|
 				templating.unpack t, unpack_path
 				Dir.chdir(File.expand_path(t, unpack_path)) do
-					execute_command_with_success('rake spec')
+					execute_command_with_success_for_template('rake spec', t)
 				end
 			}
 		end
