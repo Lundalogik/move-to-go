8aa0c4de39e0cd458e3cfcdeede6cca92deacffb
Anders Pålsson <apl@lundalogik.se>
2014-05-14 16:03:46 +0200

    Fixed failing tests

--- templates/excel/spec/tomodel_spec.rb
+++ templates/excel/spec/tomodel_spec.rb
@@ -3,16 +3,16 @@
 require 'spec_helper'
 require 'tomodel'
 
-
 describe 'Model' do
     before(:all) do
-        toModel = ToModel.new
-        samplefile =File.join(File.dirname(__FILE__), 'sample_data', 'sample.xlsx')
-        @model = toModel.to_model(samplefile)        
+        converter = Converter.new
+        samplefile = File.join(File.dirname(__FILE__), '..', 'template.xlsx')
+        @rootmodel = converter.to_model(samplefile)
     end
+
     it "will find something with a name" do
-        organization = @model.organizations[0]
+        organization = @rootmodel.organizations[0]
         organization.name.length.should > 0
-    end    
+    end
 end
 
