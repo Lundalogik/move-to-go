758179c1ece03c6d8a281ef2d45cc0d5a97a6f47
Anders Pålsson <apl@lundalogik.se>
2014-09-10 11:32:56 +0200

    Added VISMA source

---
+++ sources/VISMA/.go_import/runner.rb
@@ -0,0 +1,77 @@
+# encoding: UTF-8
+
+require 'go_import'
+#require 'roo'
+require 'dbf'
+require_relative("../converter")
+
+KUND_FILE = './database/KUND.DBF'
+KONTAKT_FILE = './database/KONTAKT.DBF'
+
+def convert_source
+    puts "Trying to convert VISMA source to LIME Go..."
+
+    # Verify that required files exists.
+    if !File.exists?(KUND_FILE)
+        puts "You MUST put KUND.DBF in the database folder."
+        raise
+    end
+
+    if !File.exists?(KONTAKT_FILE)
+        puts "You MUST put KONTAKT.DBF in the database folder."
+        raise
+    end
+
+    puts "Trying to read data from VISMA files..."
+    organization_rows = DBF::Table.new(KUND_FILE)
+    person_rows = DBF::Table.new(KONTAKT_FILE)
+
+    rootmodel = GoImport::RootModel.new
+    converter = Converter.new
+
+    converter.configure rootmodel
+
+    # Then create organizations, they are only referenced by
+    # coworkers.
+    puts "Trying to process Organization..."
+    organization_rows.each do |row|
+        if not row.nil?
+            if not row["NAMN"] == ""
+                rootmodel.add_organization(converter.to_organization(row, rootmodel))
+            end
+        end
+    end
+    puts "Processed #{rootmodel.organizations.length} Organizations."
+
+    # Add people and link them to their organizations
+    puts "Trying to process Persons..."
+    imported_person_count = 0
+    person_rows.each do |row|
+        # People are special since they are not added directly to
+        # the root model
+        if not row.nil?
+            if not row["KUNDNR"] == "" and not row["NAMN"] == ""
+                converter.import_person_to_organization(row, rootmodel)
+                imported_person_count = imported_person_count + 1
+            end
+        end
+    end
+    puts "Processed #{imported_person_count} Persons."
+
+    # Notes must be owned by a coworker and the be added to
+    # organizations and notes and might refernce a person
+    puts "Trying to process Notes..."
+    imported_note_count = 0
+    organization_rows.each do |row|
+        if not row.nil?
+            if row['ANTECK_1'].length > 0
+                rootmodel.add_note(converter.to_note(row))
+                imported_note_count = imported_note_count + 1
+            end
+        end
+    end
+    puts "Processed #{imported_note_count} Notes."
+
+    return rootmodel
+end
+
---
+++ sources/VISMA/converter.rb
@@ -0,0 +1,104 @@
+﻿require 'go_import'
+require 'dbf'
+
+# Customize this file to suit your input for a VISMA database.
+#
+# You must put KUND.DBS and KONTAKTER.DBS in the database folder.
+#
+# Documentation go_import can be found at
+# http://rubygems.org/gems/go_import
+#
+# go_import contains all objects in LIME Go such as organizations,
+# people, deals, etc. What properties each object has is described in
+# the documentation.
+#
+# Generate the xml-file that should be sent to LIME Go with the command:
+# go-import run
+
+class Converter
+    def configure(rootmodel)
+        # Add custom field to your model here. Custom fields can be
+        # added to organization, deal and person. Valid types are
+        # :String and :Link. If no type is specified :String is used
+        # as default.
+
+        #Creates a custom field to add invoicing data
+        rootmodel.settings.with_organization do |org|
+            org.set_custom_field( { :integrationid => 'ackoms', :title => 'Fakturerat', :type => :String } )
+        end
+    end
+
+    def import_person_to_organization(row, rootmodel)
+        organization = rootmodel.find_organization_by_integration_id(row['KUNDNR'])
+
+        if !organization.nil?
+            organization.add_employee(to_person(row, rootmodel))
+        end
+    end
+
+    def to_organization(row, rootmodel)
+        organization = GoImport::Organization.new()
+
+        #Add tags:
+        organization.set_tag "Importerad"
+        organization.set_tag "Kund"
+
+        organization.name = row['NAMN']
+        # Integrationid must be set to be able to import the same file
+        # more than once without creating duplicates
+        organization.integration_id = row['KUNDNR']
+
+        #address
+        organization.with_postal_address do |address|
+            address.street = row['POSTADR']
+            address.zip_code = row['POSTNR']
+            address.city = row['ORT']
+        end
+
+        organization.email = row['EPOST']
+        organization.organization_number = row['ORGNR']
+        organization.central_phone_number = row['TEL']
+
+        # Sets the organization's relation. Relation must be a value
+        # from GoImport::Relation.
+        organization.relation = GoImport::Relation::IsACustomer
+
+        #Fill data to custom fields
+        organization.set_custom_field({:integration_id => "ackoms", :value => row["ACKOMS"]})
+
+        return organization
+    end
+
+    def to_note(row, rootmodel)
+        note = GoImport::Note.new()
+
+        # *** TODO:
+        #
+        # Set note properties from the row.
+        organization = rootmodel.find_organization_by_integration_id(row['KUNDNR'])
+        unless organization.nil?
+            note.organization = organization
+        end
+        note.created_by = rootmodel.import_coworker
+        note.text = row['ANTECK_1']
+
+        return note
+    end
+
+    def to_person(row, rootmodel)
+        person = GoImport::Person.new()
+
+        # *** TODO:
+        #
+        # Set person properties from the row.
+
+        person.parse_name_to_firstname_lastname_se(row['NAMN'])
+        if GoImport::EmailHelper.is_valid?(row['EPOST'])
+            person.email = row['EPOST']
+        end
+        person.mobile_phone_number = GoImport::PhoneHelper.parse_numbers(row['MBTEL'], [",", "/", "\\"])
+        person.direct_phone_number = GoImport::PhoneHelper.parse_numbers(row['TEL'], [",", "/", "\\"])
+
+        return person
+    end
+end
