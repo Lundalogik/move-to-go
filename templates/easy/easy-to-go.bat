06c40a8650378dbeda3fe4e562f1d6d36010160c
Anders Pålsson <apl@lundalogik.se>
2014-09-24 16:17:56 +0200

    Removed obsolete template. Has been converted to source

--- templates/easy/easy-to-go.bat
+++
@@ -1,8 +0,0 @@
-:: This script will convert an exported KONTAKT.mdb in the folder
-:: Export to a file that can be imported into LIME Go.
-:: The file will be named go.xml
-
-@echo off
-ruby convert.rb to_go --coworkers=Export\User.txt --organizations=Export\Company.txt --persons=Export\Company-Person.txt --orgnotes=Export\Company-History.txt --includes=Export\Project-Included.txt --deals=Export\Project.txt --dealnotes=Export\Project-History.txt  --output=go.xml
-
-
