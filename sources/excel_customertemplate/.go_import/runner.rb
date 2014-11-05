6d98b16b174231878570f1de88545c61447a34eb
rafadalbaker <rafadalbaker@gmail.com>
2014-11-05 21:49:00 +0100

    Template for customer go-import

---
+++ sources/excel_customertemplate/.go_import/runner.rb
@@ -0,0 +1,138 @@
+# encoding: UTF-8
+
+require 'go_import'
+require 'roo'
+require_relative("../converter")
+
+def convert_source
+    puts "Trying to convert Excel source to LIME Go..."
+
+    converter = Converter.new
+
+    # First we read each sheet from the excel file into separate
+    # variables
+
+    if defined?(EXCEL_FILE) && !EXCEL_FILE.empty?()
+        if !::File.exists?(EXCEL_FILE)
+            puts "ERROR: Cant find excel file to import: '#{EXCEL_FILE}'."
+            raise
+        end
+    else
+        puts "ERROR: You must set EXCEL_FILE in converter.rb to point to the excel file that should be imported."
+        raise
+    end
+
+    excel_workbook = GoImport::ExcelHelper.Open(EXCEL_FILE)
+
+    if defined?(COWORKER_SHEET)
+        if excel_workbook.has_sheet?(COWORKER_SHEET)
+            coworker_rows = excel_workbook.rows_for_sheet COWORKER_SHEET
+        else
+            puts "WARNING: can't find sheet '#{COWORKER_SHEET}'"
+        end
+    end
+
+    if defined?(ORGANIZATION_SHEET)
+        if excel_workbook.has_sheet?(ORGANIZATION_SHEET)
+            organization_rows = excel_workbook.rows_for_sheet ORGANIZATION_SHEET
+        else
+            puts "WARNING: can't find sheet '#{ORGANIZATION_SHEET}'"
+        end
+    end
+
+    if defined?(PERSON_SHEET)
+        if excel_workbook.has_sheet?(PERSON_SHEET)
+            person_rows = excel_workbook.rows_for_sheet PERSON_SHEET
+        else
+            puts "WARNING: can't find sheet '#{PERSON_SHEET}'"
+        end
+    end
+
+    if defined?(DEAL_SHEET)
+        if excel_workbook.has_sheet?(DEAL_SHEET)
+            deal_rows = excel_workbook.rows_for_sheet DEAL_SHEET
+        else
+            puts "WARNING: can't find sheet '#{DEAL_SHEET}'"
+        end
+    end
+
+    if defined?(NOTE_SHEET)
+        if excel_workbook.has_sheet?(NOTE_SHEET)
+            note_rows = excel_workbook.rows_for_sheet NOTE_SHEET
+        else
+            puts "WARNING: can't find sheet '#{NOTE_SHEET}'"
+        end
+    end
+
+    if defined?(FILE_SHEET)
+        if excel_workbook.has_sheet?(FILE_SHEET)
+            file_rows = excel_workbook.rows_for_sheet FILE_SHEET
+        else
+            puts "WARNING: can't find sheet '#{FILE_SHEET}'"
+        end
+    end
+
+    # Then we create a rootmodel that will contain all data that
+    # should be exported to LIME Go.
+    rootmodel = GoImport::RootModel.new
+
+    # And configure the model if we have any custom fields
+    converter.configure rootmodel
+
+    # Now start to read data from the excel file and add to the
+    # rootmodel. We begin with coworkers since they are referenced
+    # from everywhere (orgs, deals, notes)
+    if defined?(coworker_rows) && !coworker_rows.nil?
+        puts "Trying to convert coworkers..."
+        coworker_rows.each do |row|
+            rootmodel.add_coworker(converter.to_coworker(row))
+        end
+    end
+
+    # Then create organizations, they are only referenced by
+    # coworkers.
+    if defined?(organization_rows) && !organization_rows.nil?
+        puts "Trying to convert organizations..."
+        organization_rows.each do |row|
+            organization = converter.to_organization(row, rootmodel)
+            rootmodel.add_organization(organization)
+        end
+    end
+
+    # Add people and link them to their organizations
+    if defined?(person_rows) && !person_rows.nil?
+        puts "Trying to convert persons..."
+        person_rows.each do |row|
+            # People are special since they are not added directly to
+            # the root model
+            converter.import_person_to_organization(row, rootmodel)
+        end
+    end
+
+    # Deals can connected to coworkers, organizations and people.
+    if defined?(deal_rows) && !deal_rows.nil?
+        puts "Trying to convert deals..."
+        deal_rows.each do |row|
+            rootmodel.add_deal(converter.to_deal(row, rootmodel))
+        end
+    end
+
+    # Notes must be owned by a coworker and the be added to
+    # organizations and notes and might refernce a person
+    if defined?(note_rows) && !note_rows.nil?
+        puts "Trying to convert notes..."
+        note_rows.each do |row|
+            rootmodel.add_note(converter.to_note(row, rootmodel))
+        end
+    end
+
+    if defined?(file_rows) && !file_rows.nil?
+        puts "Trying to convert files..."
+        file_rows.each do |row|
+            rootmodel.add_file(converter.to_file(row, rootmodel))
+        end
+    end
+
+    return rootmodel
+end
+
---
+++ sources/excel_customertemplate/converter.rb
@@ -0,0 +1,168 @@
+# encoding: UTF-8
+require 'go_import'
+require 'roo'
+
+# This Converter will convert an Excel file to a XML file that can be
+# imported into LIME Go.
+#
+# You need to customize this script to suit your Excel file.
+
+# First set the name of the Excel file to convert. It is a filename
+# relative to this folder.
+EXCEL_FILE = "Exempelfil.xlsx"
+
+COWORKER_SHEET = "Medarbetare"
+ORGANIZATION_SHEET = "Företag"
+PERSON_SHEET = "Kontaktperson"
+NOTE_SHEET = "Anteckningar"
+
+# Then you need to modify the script below according to the TODO
+# comments.
+
+# To generate the xml-file that should be sent to LIME Go with the
+# command:
+# go-import run
+
+class Converter
+    def configure(rootmodel)
+        # *** TODO: Add custom field to your rootmodel here. Custom fields
+        # can be added to organization, deal and person. Valid types
+        # are :String and :Link. If no type is specified :String is
+        # used as default.
+
+        # rootmodel.settings.with_organization do |organization|
+        #     organization.set_custom_field( { :integration_id => 'source', :title => 'Källa', :type => :Link } )
+        # end
+    end
+
+    def import_person_to_organization(row, rootmodel)
+        person = to_person(row, rootmodel)
+        organization = rootmodel.find_organization_by_integration_id(row['Kundnummer/ForetagsID'])
+
+        if !organization.nil?
+            organization.add_employee(person)
+        end
+    end
+
+    def to_coworker(row)
+        coworker = GoImport::Coworker.new()
+
+        # *** TODO:
+        #
+        # Set coworker properties from the row.
+
+        coworker.parse_name_to_firstname_lastname_se row['Namn/Titel']
+        coworker.integration_id = row['Namn/Titel']
+        if GoImport::EmailHelper.is_valid?(row['Email'])
+            coworker.email = row['Email']
+        end
+
+        return coworker
+    end
+
+    def to_organization(row, rootmodel)
+        organization = GoImport::Organization.new()
+
+        # Integrationid is typically the id in the system that we are
+        # getting the csv from. Must be set to be able to import the
+        # same file more than once without creating duplicates
+        organization.integration_id = row['Kundnummer/ForetagsID']
+
+        # Sets the organization's relation. Relation must be a value
+        # from GoImport::Relation.
+        organization.relation = GoImport::Relation::IsACustomer
+
+        # *** TODO:
+        #
+        # Set organization properties from the row.
+
+        organization.name = row['Namn']
+        organization.organization_number=row['Organisationsnummer']
+
+        # Postaladdress
+        organization.with_postal_address do |address|
+             address.street = row['Postadress']
+             address.zip_code = row['Postnummer']
+             address.city = row['Postnummer']
+        end
+
+        # Visitingaddress
+        organization.with_visit_address do |addr|
+             addr.street = row['Besoksadress']
+             addr.zip_code = row['Besokspostnummer']
+             addr.city = row['Besoksort']
+        end
+
+        # Set responsible such as
+        # organization.responsible_coworker = rootmodel.find_coworker_by_integration_id(row['Ansvarig saljare'])
+
+        # Set categorytag
+         if row['Företagskategori']
+             category = row['Företagskategori'].split(',')
+             category.each do |categorytag|
+                 organization.set_tag(categorytag.strip)
+             end
+         end
+
+        # Custom fields should be set like this.
+        # organization.set_custom_value("source", "internet")
+
+        ## LIME Go Relation.
+        # let's say that there is a option field in Easy called 'Customer relation'
+        # with the options '1.Customer', '2.Prospect' '3.Partner' and '4.Lost customer'
+
+        if row['Relation'] == 'Kund'
+        # We have made a deal with this organization.
+             organization.relation = GoImport::Relation::IsACustomer
+        elsif row['Relation'] == 'Prospekt'
+        # Something is happening with this organization, we might have
+        # booked a meeting with them or created a deal, etc.
+             organization.relation = GoImport::Relation::WorkingOnIt
+        elsif row['Relation'] == 'Tidigare kund'
+             organization.relation = GoImport::Relation::WasACustomer
+        # We had something going with this organization but we
+        # couldn't close the deal and we don't think they will be a
+        # customer to us in the foreseeable future.
+        #     organization.relation = GoImport::Relation::BeenInTouch
+        else
+             organization.relation = GoImport::Relation::NoRelation
+        end 
+
+        return organization
+    end
+
+    def to_person(row, rootmodel)
+        person = GoImport::Person.new()
+
+        # *** TODO:
+        #
+        # Set person properties from the row.
+
+        person.parse_name_to_firstname_lastname_se(row['Namn'])
+        if GoImport::EmailHelper.is_valid?(row['Email'])
+            person.email = row['Email']
+        end
+
+        person.direct_phone_number = row['Telefon']
+        person.mobile_phone_number = row['Mobiltelefon']
+
+        person.position = row['Titel']
+
+        return person
+    end
+
+    def to_note(row, rootmodel)
+        note = GoImport::Note.new()
+
+        # *** TODO:
+        #
+        # Set note properties from the row.
+
+        note.organization = rootmodel.find_organization_by_integration_id(row['Kundnummer/ForetagsID'])
+        note.created_by = rootmodel.find_coworker_by_integration_id(row['Skapad av medarbetare'])
+        note.text = row['Textanteckningar/Historik']
+        note.date = row['Skapad den']
+
+        return note
+    end
+end
