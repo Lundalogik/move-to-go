0233565d749ff386e8895f5347ee4e183893d393
Anders Pålsson <apl@codemode.org>
2015-03-04 09:48:01 +0100

    to_organization, etc should always return an object and fixed a typo in readme

--- sources/lime-pro-basic/converter.rb
+++ sources/lime-pro-basic/converter.rb
@@ -97,6 +97,7 @@ class Converter
         # organization.name = row['name']
         # organization.organization_number = row['registrationno']
 
+        
 
         ####################################################################
         ## Bisnode ID fields
@@ -196,7 +197,7 @@ class Converter
         #    organization.relation = GoImport::Relation::NoRelation
         # end
 
-        # return organization
+        return organization
     end
 
     # Reads a row from the Person table 
@@ -254,7 +255,7 @@ class Converter
 
         # person.set_custom_value("shoe_size", row['shoe size'])
 
-        # return person
+        return person
     end
 
     # Reads a row from the Business table 
@@ -301,8 +302,7 @@ class Converter
 
         # deal.set_tag("productname")
 
-        # return deal
-        
+        return deal
     end
 
     # Reads a row from the History table 
@@ -334,7 +334,7 @@ class Converter
         #   note.classification = GoImport::NoteClassification::Comment
         # end
         
-        # return note
+        return note
     end
 
     
