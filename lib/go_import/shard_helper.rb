17551740768ba37dbdb4143e006534e1f76659f4
Filip Persson <filip.persson@lundalogik.se>
2016-03-07 23:59:08 +0100

    changed handling of internal array of shards

--- lib/go_import/shard_helper.rb
+++ lib/go_import/shard_helper.rb
@@ -1,24 +1,27 @@
 module GoImport
     class ShardHelper
-            def initialize(model, shard_size=25000)
-                @shards = []
+            attr_accessor :shards, :current_shard_count
+            def initialize(shard_size=25000)
                 @shard_size = shard_size
                 @current_shard = GoImport::RootModel.new
+                @shards = [@current_shard]
                 @current_shard_count = 0
+            end
+
+            def shard_model(model)
                 @current_shard.configuration = model.configuration
+                @current_shard.coworkers = model.coworkers
+                @current_shard_count = model.coworkers.length
+
+                model.organizations.each{|key, org| add_organization(org)}
+                model.deals.each{|key, deal| add_deal(deal)}
+                model.notes.each{|key, note| add_note(note)}
+                add_documents(model.documents)
+
             end
 
             def get_shards()
-                if not @current_shard_count == 0
-                    @shards.push(@current_shard)
-                    return shards
-                end
-            end
-
-            def add_coworker(coworker)
-                check_or_create_new_chard()
-                @current_shard.add_coworker(coworker)
-                @current_shard_count += 1
+                return @shards
             end
 
             def add_note(note)
@@ -64,9 +67,8 @@ module GoImport
             private
             def check_or_create_new_chard()
                 if @current_shard_count > @shard_size
-                    @shards.push(@current_shard)
-                    puts "New chard created (#{shards.length})"
                     @current_shard = GoImport::RootModel.new
+                    @shards.push(@current_shard)
                     @current_shard_count = 0
                 end
             end
