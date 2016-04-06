3d64a60dc425922697550c470f25b02c00cda8e0
Petter Sandholdt <petter.sandholdt@lundalogik.se>
2016-04-06 16:20:38 +0200

    Fix so that you can specify encoding in CSV

--- sources/csv/.go_import/runner.rb
+++ sources/csv/.go_import/runner.rb
@@ -4,8 +4,8 @@ require_relative("../converter")
 
 # COWORKER_FILE and other file names should be defined ../converter.rb
 
-def process_rows(file_name)
-    data = File.open(file_name, 'r').read.encode('UTF-8',"ISO-8859-1")
+def process_rows(file_name, source_encoding)
+    data = File.read(file_name, :encoding => source_encoding)
     rows = GoImport::CsvHelper::text_to_hashes(data)
     rows.each do |row|
         yield row
@@ -22,19 +22,20 @@ def convert_source
     rootmodel = GoImport::RootModel.new
 
     converter.configure(rootmodel)
+    source_encoding = defined?(SOURCE_ENCODING) ? SOURCE_ENCODING : 'ISO-8859-1'
 
     # coworkers
     # start with these since they are referenced
     # from everywhere....
     if defined?(COWORKER_FILE) && !COWORKER_FILE.nil? && !COWORKER_FILE.empty?
-        process_rows COWORKER_FILE do |row|
+        process_rows(COWORKER_FILE, source_encoding) do |row|
             rootmodel.add_coworker(converter.to_coworker(row))
         end
     end
 
     # organizations
     if defined?(ORGANIZATION_FILE) && !ORGANIZATION_FILE.nil? && !ORGANIZATION_FILE.empty?
-        process_rows ORGANIZATION_FILE do |row|
+        process_rows(ORGANIZATION_FILE, source_encoding) do |row|
             organization = converter.to_organization(row, rootmodel)
             rootmodel.add_organization(organization)
             converter.organization_hook(row, organization, rootmodel) if defined? converter.organization_hook
@@ -44,7 +45,7 @@ def convert_source
     # persons
     # depends on organizations
     if defined?(PERSON_FILE) && !PERSON_FILE.nil? && !PERSON_FILE.empty?
-        process_rows PERSON_FILE do |row|
+        process_rows(PERSON_FILE, source_encoding) do |row|
             # adds it self to the employer
             converter.to_person(row, rootmodel)
         end
@@ -54,7 +55,7 @@ def convert_source
     # deals can reference coworkers (responsible), organizations
     # and persons (contact)
     if defined?(DEAL_FILE) && !DEAL_FILE.nil? && !DEAL_FILE.empty?
-        process_rows DEAL_FILE do |row|
+        process_rows(DEAL_FILE, source_encoding) do |row|
             rootmodel.add_deal(converter.to_deal(row, rootmodel))
         end
     end
