00554d6d7c8862ce4bec4abdd6f4c8917eb4ce4f
Petter Sandholdt <petter.sandholdt@lime.tech>
2018-09-18 15:27:21 +0200

    Add jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,62 @@
+#!/usr/bin/env groovy
+
+pipeline {
+    agent { label 'jenkins-slave-windows'}
+    options { 
+        timestamps() 
+    }
+
+    stages {
+
+        stage('Install tooling') {
+            steps {
+                powershell '''
+                    bundle install
+                '''
+            }
+        }
+
+        stage('Unit Testing') {
+            stage('Tools unit tests') {
+                steps {
+                    powershell '''
+                        bundle exec rake spec
+                    '''
+                }
+            }
+        }
+    }
+    post {
+        fixed {
+            script {
+                if (env.BRANCH_NAME == 'master') {
+                    slackSend(message: "Job ${JOB_NAME}:${BUILD_ID} on node ${NODE_NAME} finished successfully. ${BUILD_URL}",
+                              botUser: true,
+                              color: 'good')
+                } else if (env.BRANCH_NAME.startsWith('PR')) {
+                    slackSend(message: "Job ${JOB_NAME}:${BUILD_ID} on node ${NODE_NAME} finished successfully. ${BUILD_URL}",
+                              botUser: true,
+                              color: 'good')
+                }
+            }
+        }
+
+        regression {
+            script {
+                if (env.BRANCH_NAME == 'master') {
+                    slackSend(message: "Job ${JOB_NAME}:${BUILD_ID} on node ${NODE_NAME} failed. ${BUILD_URL}",
+                              botUser: true,
+                              color: 'danger')
+                } else if (env.BRANCH_NAME.startsWith('PR')) {
+                    slackSend(message: "Job ${JOB_NAME}:${BUILD_ID} on node ${NODE_NAME} failed. ${BUILD_URL}",
+                              botUser: true,
+                              color: 'danger')
+                }
+            }
+        }
+
+        cleanup {
+            step([$class: 'WsCleanup'])
+        }
+    }
+}
